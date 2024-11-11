4979d929c1fe27f31f0763ef2cb126cab8d9e85a
hamza-ridhwan <hamzathehamzah@gmail.com>
2024-11-11 19:08:14 +0300

    Updated Readme.md

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Fetch plants data from the backend
+  useEffect(() => {
+    fetch("https://react-hooks-cc-plantshop-h11u.onrender.com/plants")
+      .then((response) => response.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  // Filter plants based on the search query
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm setPlants={setPlants} />
+      <Search setSearchQuery={setSearchQuery} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
